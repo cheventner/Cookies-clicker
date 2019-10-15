155dd6fda3693b34c3e64c94713b651b337f08e1
cheventner <va.cheventner@gmail.com>
2019-10-15 16:18:23 +0200

    dernière modification

--- javascript.js
+++ javascript.js
@@ -1,42 +1,75 @@
-
-var boutonAdd = 0;
+//bouton gauche
+var click = 0;
+// bouton droit
 var compteur = 1;
-var palier = 50;
+// calculateur bonus
+var palier = 10;
+// super bonus
+var grosBonus= 20;
+var cacher = document.getElementById('bonus');
 
 
+// afficher le multiplicateur dans le bouton de droite
+function afficherBoutonMultiplicateur(){
+document.getElementById('multiplicateur').innerHTML =  (" Multiplicateur X " +compteur+ " Prochain Bonus à " + palier);
+}
+afficherBoutonMultiplicateur();
 
 // afficher mon document par l'id 'bouton'
 function affichage() {
- document.getElementById('bouton').innerHTML = boutonAdd; 
+ document.getElementById('bouton').innerHTML = click; 
 }
 
-// fonction du 'boutonAdd' 
+// fonction du 'click' bouton gauche
 function addition(){
 	// augmenter le score par compteur
-	boutonAdd = boutonAdd + compteur;
-	console.log(boutonAdd);
+	click = click + compteur;
 	affichage ();
 }
 
-// utilisation du multiplicateur
+// utilisation du multiplicateur bouton droit
 function incrementer(){
 	// bloquer le bouton compteur jusqu'au palier 50
 	
-	 // si mon boutonAdd est inferieur à mon palier de 50
-	if (boutonAdd < palier ) {
+	 // si mon click est inferieur à mon palier de 50
+	if (click < palier ) {
 	 // alors message
 		alert('vous devez attendre le prochain bonus');
-		console.log(compteur);
 	}
 		else {
 			//sinon augmenter ma variable compteur de 1
 			compteur=compteur+1;
 			// augmentation de la difficulté du palier (bonus) 
 			palier = palier * 2;
+			//dimunution de mon score de 5 quand je clique sur mon palier
+			click = click - 5;
 
-			console.log(compteur);	
+// mets à jour le bouton de gauche 
+			affichage();
+
+// mets à jour le bouton de droite
+			afficherBoutonMultiplicateur();	
 		}
-	// mets à jour l'affichage de la fenetre 'compteur' avec la bonne valeur
- 	document.getElementById('multiplicateur').innerHTML =  compteur;
- 	console.log(compteur);
 }
+	
+
+
+// fonction bouton superbonus 200 de score 
+// incrementation automatique  chaque seconde après click sur bouton superbonus
+function superBonus(){
+
+	if (click < grosBonus ) {
+	 // alors message
+		alert('vous devez atteindre le prochain Big Bonus');
+		console.log(grosBonus);
+	}	
+		else{	
+		// incrementation automatique du compteur 	
+			var autoclick = setInterval(addition,200);
+			cacher.style.display= 'none';
+
+		}				
+}
+// fonction stop 
+ //je veux que quand je clique sur mon bouton click le setInterval s'arrete			
+		
